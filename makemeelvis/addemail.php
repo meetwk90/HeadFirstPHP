f84935960fcdb6927c51e3303a4452055afac817
walterwan90 <walterwan1990@gmail.com>
2015-12-06 22:04:39 +0800

    [Make Me Elvis] Complete project

--- makemeelvis/addemail.php
+++ makemeelvis/addemail.php
@@ -1,16 +1,60 @@
-<?php
-	$dbc = mysqli_connect("localhost", "root", "", "elvis_store")
-		or die("Error connecting to MySQL server");
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <title>Make Me Elvis - Add Email</title>
+  <link rel="stylesheet" type="text/css" href="style.css" />
+</head>
+<body>
+  <img src="blankface.jpg" width="161" height="350" alt="" style="float:right" />
+  <img name="elvislogo" src="elvislogo.gif" width="229" height="32" border="0" alt="Make Me Elvis" />
+  <p>Enter your first name, last name, and email to be added to the <strong>Make Me Elvis</strong> mailing list.</p>
 
+<?php
+if (isset($_POST["submit"])) {
 	$first_name = $_POST["firstname"];
 	$last_name = $_POST["lastname"];
 	$email = $_POST["email"];
-
-	$query = "INSERT INTO email_list VALUES ('$first_name', '$last_name', '$email')";
+    $output_form = false;
 	
-	mysqli_query($dbc, $query)
-		or die("Error querying database");
-	mysqli_close($dbc);
+	if ((!empty($first_name)) && (!empty($last_name)) && (!empty($email))) {
 
-	echo "Customer added.";
+	$dbc = mysqli_connect('localhost', 'elvis', 'elvis', 'elvis_store')
+   		or die('Error connecting to MySQL server.');
+
+	$query = "INSERT INTO email_list (first_name, last_name, email)  VALUES ('$first_name', '$last_name', '$email')";
+    
+    mysqli_query($dbc, $query)
+       	or die ('Data not inserted.');
+
+    echo 'Customer added.';
+
+    mysqli_close($dbc);
+	} else {
+		echo "Please fill out all of the information.";
+    	$output_form = true;
+	}
+
+} else {
+	$output_form = true;
+}
+
+
+if ($output_form) {
 ?>
+<form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+    <label for="firstname">First name:</label>
+    <input type="text" id="firstname" name="firstname" value="<?php $first_name; ?>" /><br />
+    <label for="lastname">Last name:</label>
+    <input type="text" id="lastname" name="lastname" value="<?php $last_name; ?>" /><br />
+    <label for="email">Email:</label>
+    <input type="text" id="email" name="email" value="<?php $email; ?>" /><br />
+    <input type="submit" name="submit" value="Submit" />
+</form>
+<?php
+}
+?>
+
+</body>
+</html>
--- makemeelvis/sendemail.php
+++ makemeelvis/sendemail.php
@@ -1,24 +1,71 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <title>Make Me Elvis - Send Email</title>
+  <link rel="stylesheet" type="text/css" href="style.css" />
+</head>
+<body>
+  <img src="blankface.jpg" width="161" height="350" alt="" style="float:right" />
+  <img name="elvislogo" src="elvislogo.gif" width="229" height="32" border="0" alt="Make Me Elvis" />
+  <p><strong>Private:</strong> For Elmer's use ONLY<br />
+  Write and send an email to mailing list members.</p>
 <?php
-$from = "walterwan1990@foxmail.com";
-$subject = $_POST["subject"];
-$text = $_POST["elvismail"];
+if (isset($_POST['submit'])) {
 
-$dbc = mysqli_connect("localhost", "root", "", "elvis_store")
-	or die("Error connecting MySQL server");
-$query = "SELECT * FROM email_list";
-$result = mysqli_query($dbc, $query)
-	or die("Error querying database.");
+	$from = "walterwan1990@foxmail.com";
+	$subject = $_POST["subject"];
+	$text = $_POST["elvismail"];
+	$output_form = false;
 
-while($row = mysqli_fetch_array($result)) {
-	$first_name = $row["first_name"];
-	$last_name = $row["last_name"];
-	$to = $row["email"];
-	$msg = "Dear $first_name $last_name, \n$text";
+	if (empty($subject)) {
+		echo "You forgot the email <strong>subject</strong>.<br />";
+		$output_form = true;
+	}
+	if (empty($text)) {
+		echo "You forgot the email <strong>body</strong>.<br />";
+		$output_form = true;
+	}
 
-	mail($to, $subject, $msg, "From:" . $from);
+	if ((!empty($subject)) && (!empty($text))) {
 
-	echo "Email sent to: " . $to . "<br />";
+		$dbc = mysqli_connect("localhost", "elvis", "elvis", "elvis_store")
+			or die("Error connecting MySQL server");
+		$query = "SELECT * FROM email_list";
+		$result = mysqli_query($dbc, $query)
+			or die("Error querying database.");
+
+		while($row = mysqli_fetch_array($result)) {
+			$first_name = $row["first_name"];
+			$last_name = $row["last_name"];
+			$to = $row["email"];
+			$msg = "Dear $first_name $last_name, \n$text";
+
+			mail($to, $subject, $msg, "From:" . $from);
+
+			echo "Email sent to: " . $to . "<br />";
+		}
+
+		mysqli_close($dbc);
+	} 
+}
+else {
+	$output_form = true;
 }
 
-mysqli_close($dbc);
+if ($output_form) {
 ?>
+  <br />	
+  <form method="post" action="<?php $_SERVER['PHP_SELF']; ?>">
+    <label for="subject">Subject of email:</label><br />
+    <input id="subject" name="subject" type="text" size="30" value="<?php echo $subject; ?>" /><br />
+    <label for="elvismail">Body of email:</label><br />
+    <textarea id="elvismail" name="elvismail" rows="8" cols="40"><?php echo $text; ?></textarea><br />
+    <input type="submit" name="submit" value="submit" />
+  </form>
+<?php 
+}
+?>
+</body>
+</html>
